a320d4654351182c95872b482b5edfdf618a9a22
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-01-11 21:31:04 -0300

    Algumas correções até o requisito 18

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const INPUT_EMAIL = document.getElementById('email');
 const INPUT_SENHA = document.getElementById('senha');
 const BTN_ENTRAR = document.getElementById('btn-entrar');
+const INPUT_PERMISSAO = document.getElementById('agreement');
+const BTN_ENVIAR = document.getElementById('submit-btn');
 
 // Verifica se o email e a senha estão corretos para entrar
 function fazerLogin(event) {
@@ -16,5 +18,11 @@ function fazerLogin(event) {
   }
 }
 
-// Adicionando ouvinte
+// Ativa/desativa o botão de enviar se o checkbox de permissão for ativado/desativado
+function ativarDesativarBtnEnviar() {
+  BTN_ENVIAR.disabled = !INPUT_PERMISSAO.checked;
+}
+
+// Adicionando ouvintes
 BTN_ENTRAR.addEventListener('click', fazerLogin);
+INPUT_PERMISSAO.addEventListener('input', ativarDesativarBtnEnviar);
