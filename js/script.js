88f14cd5d110ba59bb88285845adadb032b5fc95
Jonathan R. Andrade <jonathanreisdeandrade@gmail.com>
2022-12-20 18:28:12 -0300

    Updates

---
+++ js/script.js
@@ -0,0 +1,85 @@
+// Elementos HTML
+const INPUT_EMAIL = document.getElementById('email');
+const INPUT_SENHA = document.getElementById('senha');
+const BTN_ENTRAR = document.getElementById('btn-entrar');
+const INPUT_PERMISSAO = document.getElementById('agreement');
+const BTN_ENVIAR = document.getElementById('submit-btn');
+const TEXTAREA = document.getElementById('textarea');
+const TEXTAREA_COUNTER = document.getElementById('counter');
+
+// Verifica se o email e a senha estão corretos para entrar
+function fazerLogin(event) {
+  event.preventDefault();
+  const email = INPUT_EMAIL.value;
+  const senha = INPUT_SENHA.value;
+
+  if (email === 'tryber@teste.com' && senha === '123456') {
+    window.alert('Olá, Tryber!');
+  } else {
+    window.alert('Email ou senha inválidos.');
+  }
+}
+
+// Ativa/desativa o botão de enviar se o checkbox de permissão for ativado/desativado
+function ativarDesativarBtnEnviar() {
+  BTN_ENVIAR.disabled = !INPUT_PERMISSAO.checked;
+}
+
+// Atualiza o contador do textarea
+function atualizarContadorTextarea() {
+  TEXTAREA_COUNTER.textContent = 500 - TEXTAREA.value.length;
+}
+
+// Retorna as matérias selecionadas
+function obterMaterias(form) {
+  const subjects = form.subject;
+  const retorno = [];
+  subjects.forEach((subject) => {
+    if (subject.checked) {
+      retorno.push(subject.value);
+    }
+  });
+  return retorno.join(', ');
+}
+
+// Retorna um objeto com os dados no formulário
+function obterDadosFormulario(form) {
+  const dados = {
+    Nome: `${form.name.value} ${form.lastname.value}`,
+    Email: form.email.value,
+    Casa: form.house.value,
+    Família: form.family.value,
+    Matérias: obterMaterias(form),
+    Avaliação: form.rate.value,
+    Observações: form.comment.value,
+  };
+  return dados;
+}
+
+// Mostra os dados do formulário quando clicar no botão enviar
+function mostrarDadosFormulario(event) {
+  if (event.target.textContent === 'Voltar') {
+    return;
+  }
+
+  const { form } = event.target;
+  const dados = obterDadosFormulario(form);
+  form.innerHTML = '';
+
+  Object.entries(dados).forEach((entrie) => {
+    const p = document.createElement('p');
+    p.innerHTML = `<span>${entrie[0]}:</span> ${entrie[1]}`;
+    form.appendChild(p);
+    form.appendChild(document.createElement('hr'));
+  });
+
+  event.target.textContent = 'Voltar';
+  form.appendChild(event.target);
+  event.preventDefault();
+}
+
+// Adicionando ouvintes
+BTN_ENTRAR.addEventListener('click', fazerLogin);
+INPUT_PERMISSAO.addEventListener('input', ativarDesativarBtnEnviar);
+TEXTAREA.addEventListener('input', atualizarContadorTextarea);
+BTN_ENVIAR.addEventListener('click', mostrarDadosFormulario);
